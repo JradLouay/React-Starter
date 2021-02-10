3bb6d045e1b4f80c8dcf4d1a8d8d2859484de7a0
Louay Jrad <jr.Louay@dlines.local>
2021-02-10 12:21:14 +0100

    remove moien specification

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,26 +2,7 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontFamily: {
-     
-    },
-    extend: {
-      colors: {
-        primary: {
-          blue: "#010D3C", // headings and backgrounds
-          green: "#40DCB6", //Actions (texts/symbols/..)
-          grey3: "#808093", // Pragraphs and headings
-          grey2: "#A6A6BB", // Headings
-          grey1: "#CACAD8", // headings/icon
-        },
-        secondary: {
-          blue: "#DAE8FF",
-          yellow: "#FEF1D1",
-          lightRed: "#FFECEA",
-        },
-      },
-
-    },
+    extend: {},
   }, 
   variants: {
     extend: {},
